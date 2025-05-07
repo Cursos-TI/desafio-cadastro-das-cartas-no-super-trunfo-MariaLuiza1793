cf10dfa14131b13187dc4f318768799934db635b
MariaLuiza1793 <marialuizamendesdasilva6@gmail.com>
2025-05-07 17:26:26 -0300

    cartas super trunfo maria luiza

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,51 @@
 #include <stdio.h>
-
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
+// Definição da struct para armazenar os dados 
+struct Cidade {
+    char codigo [10];
+    char nome[50];
+    int população;
+    float area;
+    float pib;
+    int pontosTuristicos;
+};
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+//Carta 1 - Rio de Janeiro
+struct Cidade cidade = {
+    "C01",              //codigo
+    "Rio de Janeiro",   //nome
+    6748000,            //população
+    1182.3,            //area
+    364.5,             //PIB 
+    25                //pontosturisticos
 
-    return 0;
+
+};
+//Carta 2 - Salvador
+struct Cidade cidade2 = {
+    "C02",         //Codigo
+    "Salvador",   // nome
+    2887000,      //população
+    693.8,        //area
+    85.2,         //PIN
+    18           //pontosturisticos
+};
+
+//Exibindo os dados da carta 1
+printf("\n--- Carta da Cidade Cadastrada ---\n");
+printf("Codigo:%s\n", cidade1.codigo);
+printf("nome:%s\n", cidade1.nome);
+printf("população:%\n", cidade1.população);
+printf("area: %.2f km²\n", cidade1.pib);
+printf("Pontos Turisticos: %d\n, cidade1.pontosTuristicos"); 
+
+//Exibindo dados da carta 2 
+printf("\n--- Carta2 ---\n");
+printf("Codigo: %s\n", cidade2.codigo);
+printf("Nome: %s\n", cidade2.nome);
+printf("População: %d\n", cidade2.população);
+printf("Área: %.2f km²\n";cidade2.Área);
+printf("PIB: %.2f bilhões\n, cidade2.pin");
+printf("Pontos Turisticos: %d\n", cidade2.pontosTuristicos);
+
+return 0;
 }
